53eeecda1d238136b308d62cc7161f00f8c0263e
Michael <mrperrins@users.noreply.github.com>
2020-08-03 17:08:59 -0700

    Update dockerize-busybox-httpd.sh

--- dockerize-busybox-httpd.sh
+++ dockerize-busybox-httpd.sh
@@ -4,6 +4,6 @@ set -e
 dockerize -t mrperrins/rpi-bagelism-httpd \
    --add-file index.html /www/ \
    --add-file bagels.jpg /www/ \
-   --entry '/bin/bagelism' \
+   --entry '/bin/busybox' \
    --cmd 'httpd -f -p 80 -h /www' \
-   /bin/bagelism
+   /bin/busybox
